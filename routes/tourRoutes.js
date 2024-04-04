2664e514ea772b7031f41456e7edb7097df0de68
duylesales <80103322+duylesales@users.noreply.github.com>
2024-04-04 19:30:40 +0700

    Making the API Better: Aliasing

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -5,6 +5,10 @@ const router = express.Router();
 
 // router.param('id', tourController.checkID);
 
+router
+  .route('/top-5-cheap')
+  .get(tourController.aliasTopTours, tourController.getAllTours);
+
 // Create a checkBody middleware
 // Check if body contains the name and the price property
 // If not, send back 400 (bad request)
