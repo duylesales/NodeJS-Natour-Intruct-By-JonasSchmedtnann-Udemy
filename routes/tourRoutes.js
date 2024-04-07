4e15b34f6c477324976a277a619c3fc99be8d703
duylesales <80103322+duylesales@users.noreply.github.com>
2024-04-07 16:41:25 +0700

    Aggregation Pipeline: Matching & Grouping

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -9,10 +9,7 @@ router
   .route('/top-5-cheap')
   .get(tourController.aliasTopTours, tourController.getAllTours);
 
-// Create a checkBody middleware
-// Check if body contains the name and the price property
-// If not, send back 400 (bad request)
-// Add it to the post handler stack
+router.route('/tour-stats').get(tourController.getTourStats);
 
 router
   .route('/')
